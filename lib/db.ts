aa31b772a1d22c17dfb4c2ac499d890a384a14c4
Adhithya Srivatsan <addytrunks@gmail.com>
2023-08-28 19:28:00 +0530

    fix:db

--- lib/db.ts
+++ lib/db.ts
@@ -6,4 +6,4 @@ declare global {
 
 export const db = global.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV === "development") global.prisma = prisma;
+if(process.env.NODE_ENV === "development") global.prisma = db;

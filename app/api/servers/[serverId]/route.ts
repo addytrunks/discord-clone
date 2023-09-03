7bad783e9e6ec722f9bf5282d113e417683561ee
Adhithya Srivatsan <addytrunks@gmail.com>
2023-09-03 13:47:54 +0530

    feat: server settings

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,38 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { serverId: string } }
+) => {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const server = await db.server.findFirst({
+      where: {
+        id: params.serverId,
+      },
+    });
+
+    const updateServer = await db.server.update({
+      where: {
+        id: params.serverId,
+        // Only an admin can update the server
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(updateServer, { status: 200 });
+  } catch (error) {
+    console.log("[SERVERID_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -49,7 +49,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
 
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem onClick={() => onOpen("editServer",{server})} className="px-3 py-2 text-sm cursor-pointer">
             Server Settings
             <Settings className="h-4 2-4 ml-auto" />
           </DropdownMenuItem>

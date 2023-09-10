ed5f27fc8f89335bbf28c4b4e0ba3db77df4f33a
Adhithya Srivatsan <addytrunks@gmail.com>
2023-09-10 11:14:51 +0530

    fix: current member bg-color

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -35,7 +35,7 @@ const ServerMember = ({ member, server }: ServerMemberProps) => {
       onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-gray-700/10 transition mb-1",
-        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-[#374151]"
       )}
     >
       <UserAvatar

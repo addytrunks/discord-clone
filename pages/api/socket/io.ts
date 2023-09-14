dc26919907b64baf04cbf9fa7a5054519368284b
Adhithya Srivatsan <addytrunks@gmail.com>
2023-09-14 20:07:45 +0530

    fix: socket io connection

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,7 +1,8 @@
 import { Server as NetServer } from "http";
 import { NextApiRequest } from "next";
 import { Server as ServerIO } from "socket.io";
-import { NextApiResponseServerIo } from "@/types.d";
+
+import { NextApiResponseServerIo } from "@/types";
 
 export const config = {
   api: {
@@ -11,7 +12,7 @@ export const config = {
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
-    const path = "/api/server/io";
+    const path = "/api/socket/io";
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
       path: path,
@@ -22,6 +23,6 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   }
 
   res.end();
-};
+}
 
 export default ioHandler;

b20ff5be6cc4bb1c9134e32bf97b6eadb2c21b35
Adhithya Srivatsan <addytrunks@gmail.com>
2023-09-17 19:03:19 +0530

    feat: message file upload modal

---
+++ components/modals/message-file-modal.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components//ui/dialog";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import FileUpload from "@/components/file-upload";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useModalStore } from "@/hooks/use-modal-store";
+import qs from "query-string";
+
+const formSchema = z.object({
+  fileUrl: z.string().min(1, {
+    message: "Attachment is required",
+  }),
+});
+
+const MessageFileModal = () => {
+  const { onClose, type, isOpen, data } = useModalStore();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "messageFile";
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fileUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: data.apiUrl! || "",
+        query:data.query!,
+      })
+      await axios.post(url, {
+        ...values,
+        content:values.fileUrl
+      });
+      router.refresh();
+      handleClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black overflow-hidden p-0">
+        <DialogHeader className="pt-8">
+          <DialogTitle className="text-3xl text-center font-bold">
+            Add an attachment
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Send a file as a message
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 p-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="fileUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          onChange={field.onChange}
+                          endpoint="messageFile"
+                          value={field.value}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button
+                variant="primary"
+                disabled={isLoading}
+                type="submit"
+                className="w-full"
+              >
+                Send
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MessageFileModal;
